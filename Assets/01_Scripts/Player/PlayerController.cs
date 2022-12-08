6c04e036b63a8f9e8cc8e00bc0dd6e61625655b8
heesoo1114 <improveme7@gmail.com>
2022-12-08 18:48:19 +0900

    가속도 추가

--- Assets/01_Scripts/Player/PlayerController.cs
+++ Assets/01_Scripts/Player/PlayerController.cs
@@ -5,12 +5,20 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     Rigidbody2D _rigidbody;
+    Animator _anim;
+    [SerializeField] AnimationCurve _animCurve;
+    [SerializeField] private float maxSpeed = 5;
+    [SerializeField] private float accelerationMaxTime = 1f;
+    private float buttonHoldTime;
+    private bool isMoving;
+
 
     public float moveSpeed = 5f;
 
     private void Awake()
     {
         _rigidbody = GetComponent<Rigidbody2D>();
+        _anim = GetComponent<Animator>();
     }
 
     private void Update()
@@ -20,6 +28,40 @@ public class PlayerController : MonoBehaviour
 
         Vector2 moveVelocity = new Vector2(x, y).normalized * moveSpeed;
 
+        moveSpeed = CalulateSpeed(moveVelocity, _animCurve);
+
         _rigidbody.velocity = moveVelocity;
+
+        AnimatorSet(moveVelocity);
+    }
+
+    private void AnimatorSet(Vector2 moveVelocity)
+    {
+        _anim.SetFloat("InputX", moveVelocity.x);
+        _anim.SetFloat("InputY", moveVelocity.y);
+    }
+
+    private void SetAccelerationParamenters(Vector2 input)
+    {
+        if (input.magnitude > 0)
+        {
+            isMoving = true;
+            buttonHoldTime += Time.deltaTime;
+        }
+        else
+        {
+            isMoving = false;
+            buttonHoldTime = 0;
+        }
+    }
+
+    float CalulateSpeed(Vector2 input, AnimationCurve anmationCurve)
+    {
+        if (isMoving)
+        {
+            float acceleration = _animCurve.Evaluate(buttonHoldTime / accelerationMaxTime);
+            return maxSpeed * acceleration;
+        }
+        else return 0;
     }
 }
