305821b08fc363fb6e31641703e4da73ed1aabb3
heesoo1114 <improveme7@gmail.com>
2022-12-23 10:04:24 +0900

    Add 'GOshooting/' from commit 'db915f7eea528b1cb8fe0836108734f999d6b58e'
    git-subtree-dir: GOshooting
    git-subtree-mainline: db915f7eea528b1cb8fe0836108734f999d6b58e
    git-subtree-split: db915f7eea528b1cb8fe0836108734f999d6b58e

---
+++ GOshooting/PlatformerGame/Assets/01_Scripts/GlobalType.cs
@@ -0,0 +1,19 @@
+namespace GlobalType
+{
+    public enum GroundType
+    {
+        none,
+        Level,
+        OneWayPlatform,
+        MovingPlatform,
+        CollapsablePlatform,
+        JumpPad
+    }
+
+    public enum WallType
+    {
+        None,
+        Normal,
+        Sticky
+    }
+}
