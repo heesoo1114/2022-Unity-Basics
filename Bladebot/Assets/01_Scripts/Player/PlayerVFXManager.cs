12fd12d1fb4422ef42b9a0c8fd53a9eb81b73f38
Yoon HeeSoo <improveme7@gmail.com>
2023-03-24 12:00:58 +0900

    엔진_0324 VFX
    footStep, Spark

--- Bladebot/Assets/01_Scripts/Player/PlayerVFXManager.cs
+++ Bladebot/Assets/01_Scripts/Player/PlayerVFXManager.cs
@@ -1,11 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Resources;
 using UnityEngine;
+using UnityEngine.VFX;
 
 public class PlayerVFXManager : MonoBehaviour
 {
     [SerializeField]
     private ParticleSystem[] _blades;
+
+    [SerializeField]
+    private VisualEffect _footStep;
 
     private AttackState _atkState;
 
@@ -14,6 +19,18 @@ public class PlayerVFXManager : MonoBehaviour
         _atkState = transform.Find("States").GetComponent<AttackState>();
         _atkState.OnAttackStart += PlayBlade;
         _atkState.OnAttackstateEnd += StopBlade;
+    }
+
+    public void UpdateFootSetp(bool state)
+    {
+        if (state)
+        {
+            _footStep.Play();
+        }
+        else
+        {
+            _footStep.Stop();
+        }
     }
 
     private void PlayBlade(int combo)
