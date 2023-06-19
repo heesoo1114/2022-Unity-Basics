a2a3081bb3d355aa91323be0ed11ea21b58cd82a
Yoon HeeSoo <improveme7@gmail.com>
2023-06-19 19:07:47 +0900

    장애물 생성 시스템 완료

--- Assets/01_Scripts/Obstacle/ObstacleMaker3.cs
+++ Assets/01_Scripts/Obstacle/ObstacleMaker3.cs
@@ -7,22 +7,42 @@ public class ObstacleMaker3 : MonoBehaviour
 {
     [SerializeField] private GameObject obstaclePrefab;
     public List<float> xPosList;
-    
+
+    [Header("[LevelDesign]")]
+    public int obstacleCntMin;
+    public int obstacleCntMax;
+    public int obstacleSpeed;
     public float delayTime;
 
     public int RandomNumber(int min, int max) => UnityEngine.Random.Range(min, max);
 
     private void Start()
     {
-        // StartCoroutine(MakeRoop());
+        StartCoroutine(MakeRoop());
     }
 
     IEnumerator MakeRoop()
     {
         while (true)
         {
-            int obsCnt = RandomNumber(1, 2);
+            int obstacleCnt = RandomNumber(0, 3);
 
+            for (int i = 0; i < obstacleCnt; i++)
+            {
+                GameObject prefab = Instantiate(obstaclePrefab);
+                prefab.transform.parent = transform;
+
+                int randomIndex = RandomNumber(obstacleCntMin, obstacleCntMax);
+                prefab.transform.position = new Vector3(xPosList[randomIndex], transform.position.y, transform.position.z);
+
+                if (prefab.GetComponent<DownMovement>() == null)
+                {
+                    prefab.AddComponent<DownMovement>();
+                }
+                prefab.GetComponent<DownMovement>().MoveSpeed = obstacleSpeed;
+            }
+
+            yield return new WaitForSeconds(delayTime);
         }
     }
 }
