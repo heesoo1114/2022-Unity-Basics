b8b5b4dcfa188d0e3a7375eba1738b46e1b305f0
Yoon HeeSoo <improveme7@gmail.com>
2023-09-07 09:14:19 +0900

    FOVStencil done

--- 2023 2학년/FOVStencil/Assets/01_Scripts/BT/SampleBrain.cs
+++ 2023 2학년/FOVStencil/Assets/01_Scripts/BT/SampleBrain.cs
@@ -1,11 +1,9 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class SampleBrain : EnemyBrain
 {
     public override void Attack()
     {
-
+        Debug.Log("АјАн!");
     }
 }
