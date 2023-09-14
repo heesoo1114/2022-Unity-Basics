d0d43432e14468a968a312909a7e778aa1c9ba19
Yoon HeeSoo <improveme7@gmail.com>
2023-09-14 09:22:00 +0900

    ui modify

--- 2023 2학년/MadeBTAsset/Assets/BTVisual/Editor/BTEditor.cs
+++ 2023 2학년/MadeBTAsset/Assets/BTVisual/Editor/BTEditor.cs
@@ -1,17 +1,23 @@
-using UnityEngine.UIElements;
-using UnityEditor.Callbacks;
-using UnityEditor;
-using UnityEngine;
-using BTVisual;
 using System;
+using BTVisual;
+using UnityEditor;
+using UnityEditor.Callbacks;
+using UnityEngine;
+using UnityEngine.UIElements;
 
 public class BTEditor : EditorWindow
 {
-    [SerializeField]
-    private VisualTreeAsset m_VisualTreeAsset = default;
+    [SerializeField] private VisualTreeAsset m_VisualTreeAsset = default;
 
     private BehaviourTreeView _treeView;
     private InspectorView _inspectorView;
+
+    [MenuItem("Window/BTEditor")]
+    public static void OpenWindow()
+    {
+        BTEditor wnd = GetWindow<BTEditor>();
+        wnd.titleContent = new GUIContent("BTEditor");
+    }
 
     [OnOpenAsset]
     public static bool OnOpenAsset(int instanceId, int line)
@@ -24,13 +30,6 @@ public class BTEditor : EditorWindow
         return false;
     }
 
-    [MenuItem("Window/BTEditor")]
-    public static void OpenWindow()
-    {
-        BTEditor wnd = GetWindow<BTEditor>();
-        wnd.titleContent = new GUIContent("BTEditor");
-    }
-
     public void CreateGUI()
     {
         VisualElement root = rootVisualElement;
@@ -39,15 +38,15 @@ public class BTEditor : EditorWindow
         template.style.flexGrow = 1;
         root.Add(template);
 
+        var styleSheet = AssetDatabase.LoadAssetAtPath<StyleSheet>("Assets/BTVisual/Editor/BTEditor.uss");
+        root.styleSheets.Add(styleSheet);
+
         _treeView = root.Q<BehaviourTreeView>("tree-view");
         _inspectorView = root.Q<InspectorView>("inspector-view");
 
         _treeView.OnNodeSelected += OnSelectionNodeChanged;
-        
-        var styleSheet = AssetDatabase.LoadAssetAtPath<StyleSheet>("Assets/BTVisual/Editor/BTEditor.uss");
-        root.styleSheets.Add(styleSheet);
 
-        OnSelectionChange();
+        OnSelectionChange(); //강제로 호출해서 
     }
 
     private void OnSelectionNodeChanged(NodeView nv)
@@ -55,12 +54,11 @@ public class BTEditor : EditorWindow
         _inspectorView.UpdateSelection(nv);
     }
 
-    // 마우스로 클릭하여 무엇을 선택하였을 때 실행
     private void OnSelectionChange()
     {
         var tree = Selection.activeObject as BehaviourTree;
 
-        if (tree != null && AssetDatabase.CanOpenAssetInEditor(tree.GetInstanceID()))
+        if (tree != null)
         {
             _treeView.PopulateView(tree);
         }
